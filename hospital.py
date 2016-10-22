9fe72ed47bf69137355e63b94258778677fef31d
modelarious <michaelwilliamhackman@gmail.com>
2016-10-22 16:24:04 -0600

    Added time stamps and modified function of hospital.py and build_database.py
    Added the ability to supply a data path in the command line, or leave
    it blank for our test data as well as updating the fake test data with
    time stamps to meet the new requirements.

--- hospital.py
+++ hospital.py
@@ -1,9 +1,15 @@
 import sqlite3
-from Build_database import build_database
-
+import sys
+from Build_Database import Build_Database
+'''
+When run from terminal, type $ python2 hospital.py ./My_different_data.sql to load the database with alternate data.
+if no second path is supplied, data will default to our fake test data
+'''
+def main(argv):
+    if len(argv) > 1:
+        Build_Database(argv[1])
+    else:
+        Build_Database()
+        
 if __name__ == '__main__':
-    sqldb = './hospital.db'
-    conn = sqlite3.connect(sqldb)
-    c = conn.cursor()
-
-    build_database(conn, c)
+    main(sys.argv)
