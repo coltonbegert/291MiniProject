97d67eb16e97c148cf5001c02689ae2c10043174
modelarious <michaelwilliamhackman@gmail.com>
2016-10-28 00:25:55 -0600

    fixed a few bugs and commented Build_Database and utilities

--- Build_database.py
+++ Build_database.py
@@ -4,18 +4,19 @@ import os
 def Build_Database(test_data_path = './a3-data.sql'):
     conn = sqlite3.connect('./hospital.db')
     c = conn.cursor()
-    c.execute(' PRAGMA forteign_keys=ON; ')
+    
+    c.execute(' PRAGMA forteign_keys=ON; ') ## WHY DOES THIS STATEMENT WORK????? foreign is mispelled, but if I spell it as foreign, I get the error: sqlite3.IntegrityError: FOREIGN KEY constraint failed
     conn.commit()
 
     with open('./hospital.sql', 'r') as f:  #build the database structure
             hospital_schema = f.read()    
     c.executescript(hospital_schema)
     conn.commit()
-    with open(test_data_path, 'r') as f: #add the test data to the database
+    
+    
+    with open(test_data_path, 'r') as f: #add the test data to the database based on the test_data_path
         test_data = f.read()
     c.executescript(test_data)
     conn.commit()
     conn.close()
     return 0
-
-Build_Database()
