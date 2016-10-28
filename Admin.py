733bfc8c19ab59dc8bcf36f14a54fb7e5eadc74b
coltonbegert <coltonbegert@gmail.com>
2016-10-28 10:13:25 -0600

    Fixed the fourth option for admin and commented the file

--- Admin.py
+++ Admin.py
@@ -1,10 +1,11 @@
 import sqlite3
 
 def Admin():
+    # Present the options for admin staff and loop for valid response
     while 0==0:
         option = raw_input("What would you like to do?\n(1) Create Report\n(2) Prescription information about a drug\n(3) Medication recommendation\n(4) Search for drug applications\n(5) Logout\n:")
         if option == '1':
-            create_report()
+            drug_prescribed()
         elif option == '2':
             prescribed_drugs()
         elif option == '3':
@@ -15,18 +16,19 @@ def Admin():
             return 0
         else:
             print "\nInvalid option\n"
-
+# connects to the sql databse
 def connect_db(db_name = "./hospital.db"):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
     return conn, c
-
+# disconecnts for the sql database
 def disconnect_db(conn):
     conn.commit()
     conn.close()
     return
-
-def create_report():
+# the first option for admin creates a report of the amount of each drug prescribed over a time period
+# The amount is the amount per day x the number of days taking the drug
+def drug_prescribed():
     print "Define time period"
     start_date = raw_input("Enter start of time period (YYYY-MM-DD): ")
     end_date = raw_input("Enter end of time period (YYYY-MM-DD): ")
@@ -34,6 +36,10 @@ def create_report():
     # end_date = 'now'
     conn, c = connect_db()
 
+# SQL statement for pulling the amount and name of each drug that a doctor prescribes over a time period.
+# Assumptions: reolution of 1 day, amount = daily dose x number of days prescribed,
+#              amount is the total amount even if perion is larger than specified time period
+# grouped by the doctors name
     c.execute('''SELECT s.name, m.drug_name, sum(m.amount* (cast(julianday(m.end_med) as int) - cast(julianday(m.start_med) as int)))
         FROM staff s, medications m
         WHERE m.staff_id = s.staff_id
@@ -42,6 +48,8 @@ def create_report():
         HAVING s.role = 'D'
         ORDER BY s.name;''', (start_date,end_date))
 
+# uses the values returned form the sql statement
+# only prints doctors name once and indents all drugs and the total amount of all medication prescribed in range
     result = c.fetchall()
     last_name = ""
     for row in result:
@@ -52,18 +60,25 @@ def create_report():
 
 
 
-
+# closes db connection
     disconnect_db(conn)
+
+
+# Second option for admin, makes a report of the amount of each drug prescribed in a time period
 
 def prescribed_drugs():
     conn,c = connect_db()
-
+# gets user input for date range
     start_date = raw_input("Enter start of time period (YYYY-MM-DD): ")
     end_date = raw_input("Enter end of time period (YYYY-MM-DD): ")
     # start_date = '2000-01-01'
     # end_date = 'now'
     conn, c = connect_db("./hospital.db")
 
+# SQL statement for getting the report
+# grouped by drug drug category and the drug name
+# the complicated part of this is the 4th column that returns the amount of the drug prescribed for a specific category
+# Assumptions: Amount once again is the amount per day X the number of days
     c.execute('''SELECT d.category, m.drug_name, sum(m.amount* (cast(julianday(m.end_med) as int) - cast(julianday(m.start_med) as int))), (
             SELECT sum(m1.amount* (cast(julianday(m1.end_med) as int) - cast(julianday(m1.start_med) as int))) as total
             FROM medications m1, drugs d1
@@ -77,6 +92,8 @@ def prescribed_drugs():
         ORDER BY d.category;''', (start_date,end_date,start_date,end_date))
 
     result = c.fetchall()
+
+    # formats and prints the SQL results in  a nice way by displaying the category only once
     last_name = ""
     for row in result:
         if row[0] != last_name:
@@ -88,11 +105,14 @@ def prescribed_drugs():
 
     disconnect_db(conn)
 
+# Third option for admin, displays all medications that have been used for a diagnoses
 def med_recommend():
     conn,c = connect_db()
 
     diagnoses = raw_input("Enter a diagnoses: ")
-
+# SQL Staement for getting the drugs that have been prescribed after a diagnoses
+# COLLATE NOCASE makes the drug names case insensitive for search purposes
+# gorupby and order by gives us an easy way to split each use of the drug for a diagnoses and order them by frequency
     c.execute('''SELECT m.drug_name
         FROM charts c, diagnoses d,medications m
         WHERE c.chart_id = d.chart_id
@@ -104,10 +124,12 @@ def med_recommend():
     )
 
     result = c.fetchall()
+    # This problem is a simple print as it just wants the drug names
     for row in result:
         print row[0]
     disconnect_db(conn)
-
+# Fourth option for admnin staff, creates a report of the amounts of each use of a drug for different diagnoses
+# Prints average dosing information for length, daily dose and the total that it has been prescribed
 def drug_uses():
     conn,c = connect_db()
 
@@ -116,25 +138,24 @@ def drug_uses():
     # http://stackoverflow.com/questions/4517681/sql-sum-with-condition
     # drug = 'ZMapp'
 
-    c.execute('''SELECT d.diagnosis || (select  cast(((SUM(CASE WHEN m1.drug_name = m.drug_name THEN 1 ELSE 0 END)+0.0)/(count(*)+0.0)) as float)
-      FROM medications m1, diagnoses d1
-      WHERE m1.chart_id = d1.chart_id
-      AND m1.hcno = d1.hcno
-      AND d1.diagnosis = d.diagnosis)
+# SQL Statemnt for getting the data.
+# Uses the same method for determing the duration and total amounts of drugs prescribed for option 1(create)
+# groups by the diagnoses and orders using an AS from the SELECT
+    c.execute('''SELECT d.diagnosis, m.amount, (cast(julianday(m.end_med) as int) - cast(julianday(m.start_med) as int)), avg(m.amount* (cast(julianday(m.end_med) as int) - cast(julianday(m.start_med) as int))) as prescribeAverage
         FROM charts c, diagnoses d,medications m
         WHERE c.chart_id = d.chart_id
         AND m.chart_id = c.chart_id
         AND m.drug_name = ? COLLATE NOCASE
         AND m.mdate > d.ddate
         GROUP BY d.diagnosis
-        ORDER BY (select  cast((SUM(CASE WHEN m1.drug_name = m.drug_name THEN 1 ELSE 0 END)/count(*)) as float)
-          FROM medications m1, diagnoses d1
-          WHERE m1.chart_id = d1.chart_id
-          AND m1.hcno = d1.hcno
-          AND d1.diagnosis = d.diagnosis) DESC;
+        ORDER BY prescribeAverage DESC;
 
     ''', ([drug]))
 
+# fancy printing only to make the results nicely readable.
     result = c.fetchall()
     for row in result:
-        print row[0]
+        # print row[0] + ' Taking an average' + row[1] + 'per day for' row[2] + 'days totaling ' +row[2] + 'prescribed'
+        print '%s: Taking an average %d units per day for %d days totaling %d units prescribed.' %(row[0],row[1],row[2],row[3])
+
+# drug_uses()
